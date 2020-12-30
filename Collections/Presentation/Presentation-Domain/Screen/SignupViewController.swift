e3003e92d9b3bd3b079d23a38d616d3d5dcdabba
yossibank <yossibank@yahoo.co.jp>
2020-12-30 18:10:20 +0900

    追加: keyboardNotifierファイル作成

--- Collections/Presentation/Presentation-Domain/Screen/SignupViewController.swift
+++ Collections/Presentation/Presentation-Domain/Screen/SignupViewController.swift
@@ -2,11 +2,14 @@ import UIKit
 
 final class SignupViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordConfirmationTextField: UITextField!
     @IBOutlet weak var secureTextChangeButton: UIButton!
     @IBOutlet weak var signupButton: UIButton!
+
+    var keyboardNotifier: KeyboardNotifier = KeyboardNotifier()
 
     private var isSecureCheck: Bool = true
 
@@ -14,6 +17,7 @@ final class SignupViewController: UIViewController {
         super.viewDidLoad()
         setupTextField()
         setupButton()
+        listenerKeyboard(keyboardNotifier: keyboardNotifier)
     }
 }
 
@@ -70,5 +74,19 @@ extension SignupViewController: UITextFieldDelegate {
             textField.resignFirstResponder()
         }
         return true
+    }
+}
+
+extension SignupViewController: KeyboardDelegate {
+
+    func keyboardPresent(_ height: CGFloat) {
+        let displayHeight = self.view.frame.height - height
+        let signupButtonOffsetY = signupButton.convert(signupButton.frame, to: stackView).minY
+        let bottomOffsetY = signupButtonOffsetY - displayHeight
+        view.frame.origin.y == 0 ? (view.frame.origin.y -= bottomOffsetY) : ()
+    }
+
+    func keyboardDismiss(_ height: CGFloat) {
+        view.frame.origin.y != 0 ? (view.frame.origin.y = 0) : ()
     }
 }
---
+++ Collections/Presentation/Utility/Service/KeyboardNotifier.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+final class KeyboardNotifier {
+
+    var keyboardPresent: ((_ height: CGFloat) -> Void)?
+    var keyboardDismiss: ((_ height: CGFloat) -> Void)?
+
+    func listenKeyboard() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillChangeFrameX),
+            name: UIApplication.keyboardWillChangeFrameNotification,
+            object: nil
+        )
+    }
+
+    @objc private func keyboardWillChangeFrameX(_ notification: Notification) {
+        if let endFrame = (
+            notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+        )?.cgRectValue {
+            let keyboardHeight = UIScreen.main.bounds.height - endFrame.origin.y
+            if keyboardHeight > 0 {
+                self.keyboardPresent?(keyboardHeight)
+            } else {
+                self.keyboardDismiss?(keyboardHeight)
+            }
+        }
+    }
+}
+
+protocol KeyboardDelegate: class {
+    
+    var keyboardNotifier: KeyboardNotifier { get set }
+    
+    func keyboardPresent(_ height: CGFloat)
+    func keyboardDismiss(_ height: CGFloat)
+}
+
+extension KeyboardDelegate {
+    
+    func listenerKeyboard(keyboardNotifier: KeyboardNotifier) {
+        keyboardNotifier.listenKeyboard()
+        
+        keyboardNotifier.keyboardPresent = { height in
+            self.keyboardPresent(height)
+        }
+        
+        keyboardNotifier.keyboardDismiss = { height in
+            self.keyboardDismiss(height)
+        }
+    }
+}
