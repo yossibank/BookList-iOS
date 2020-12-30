5e67082066ecece3b27faf7f2c390bb2c2005a97
yossibank <yossibank@yahoo.co.jp>
2020-12-30 10:22:16 +0900

    initial commit

---
+++ Collections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collections
+//
+//  Created by KAMIYAMA YOSHIHITO on 2020/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
