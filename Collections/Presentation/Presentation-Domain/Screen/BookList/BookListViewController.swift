56f66c2118b580f458a122be5711c6735a48a408
yossibank <yossibank@yahoo.co.jp>
2021-01-11 12:31:51 +0900

    作成: 書籍一覧セルUIベース

--- Collections/Presentation/Presentation-Domain/Screen/BookList/BookListViewController.swift
+++ Collections/Presentation/Presentation-Domain/Screen/BookList/BookListViewController.swift
@@ -12,3 +12,35 @@ final class BookListViewController: UIViewController {
         super.viewDidLoad()
     }
 }
+
+extension BookListViewController {
+
+    private func setupTableView() {
+        tableView.register(BookListTableViewCell.xib(), forCellReuseIdentifier: BookListTableViewCell.resourceName)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension BookListViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+extension BookListViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: BookListTableViewCell.resourceName,
+            for: indexPath
+        ) as? BookListTableViewCell ?? BookListTableViewCell()
+
+        return cell
+    }
+}
--- Collections/Presentation/Presentation-Domain/Screen/BookList/View/BookListTableViewCell.swift
+++ Collections/Presentation/Presentation-Domain/Screen/BookList/View/BookListTableViewCell.swift
@@ -2,11 +2,23 @@ import UIKit
 
 final class BookListTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var bookPurchaseLabel: UILabel!
+    @IBOutlet weak var bookPriceLabel: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+
+    func setup(image: UIImage, title: String, purchaseDate: String, price: String) {
+        bookImageView.image = image
+        bookTitleLabel.text = title
+        bookPurchaseLabel.text = purchaseDate
+        bookPriceLabel.text = price
     }
 }
