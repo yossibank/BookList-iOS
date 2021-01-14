b12b591141c4e970d08833bb1ad1517310f7c23c
yossibank <yossibank@yahoo.co.jp>
2021-01-15 00:03:00 +0900

    追加: 書籍追加アウトレット接続

--- Collections/Presentation/Presentation-Domain/Screen/Book/AddBook/AddBookViewController.swift
+++ Collections/Presentation/Presentation-Domain/Screen/Book/AddBook/AddBookViewController.swift
@@ -2,6 +2,13 @@ import UIKit
 
 final class AddBookViewController: UIViewController {
 
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var imageSelectButton: UIButton!
+    @IBOutlet weak var takingPictureButton: UIButton!
+    @IBOutlet weak var bookTitleTextField: UITextField!
+    @IBOutlet weak var bookPriceTextField: UITextField!
+    @IBOutlet weak var bookPurchaseDateTextField: UITextField!
+
     static func createInstance() -> AddBookViewController {
         AddBookViewController.instantiateInitialViewController()
     }
