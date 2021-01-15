fe10b53702bffea0a37e424aa1163bcef6afd680
yossibank <yossibank@yahoo.co.jp>
2021-01-15 21:18:40 +0900

    追加: 書籍追加テキストフィールドキーボード処理

--- Collections/Presentation/Presentation-Domain/Screen/Book/AddBook/AddBookViewController.swift
+++ Collections/Presentation/Presentation-Domain/Screen/Book/AddBook/AddBookViewController.swift
@@ -2,12 +2,15 @@ import UIKit
 
 final class AddBookViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var bookImageView: UIImageView!
     @IBOutlet weak var imageSelectButton: UIButton!
     @IBOutlet weak var takingPictureButton: UIButton!
     @IBOutlet weak var bookTitleTextField: UITextField!
     @IBOutlet weak var bookPriceTextField: UITextField!
     @IBOutlet weak var bookPurchaseDateTextField: UITextField!
+
+    var keyboardNotifier: KeyboardNotifier = KeyboardNotifier()
 
     static func createInstance() -> AddBookViewController {
         AddBookViewController.instantiateInitialViewController()
@@ -16,6 +19,7 @@ final class AddBookViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTextField()
+        listenerKeyboard(keyboardNotifier: keyboardNotifier)
     }
 }
 
@@ -42,5 +46,22 @@ extension AddBookViewController: UITextFieldDelegate {
             textField.resignFirstResponder()
         }
         return true
+    }
+}
+
+extension AddBookViewController: KeyboardDelegate {
+
+    func keyboardPresent(_ height: CGFloat) {
+        let displayHeight = self.view.frame.height - height
+        let bottomOffsetY = stackView.convert(
+            bookPurchaseDateTextField.frame,
+            to: self.view
+        ).maxY + 20 - displayHeight
+
+        view.frame.origin.y == 0 ? (view.frame.origin.y -= bottomOffsetY) : ()
+    }
+
+    func keyboardDismiss(_ height: CGFloat) {
+        view.frame.origin.y != 0 ? (view.frame.origin.y = 0) : ()
     }
 }
