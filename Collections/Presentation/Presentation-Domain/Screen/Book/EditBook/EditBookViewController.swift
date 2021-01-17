ac35e67f1216d71b97cdc868cfa8ceff6e8d5cdd
yossibank <yossibank@yahoo.co.jp>
2021-01-17 10:06:51 +0900

    追加: 書籍編集ボタン設定

--- Collections/Presentation/Presentation-Domain/Screen/Book/EditBook/EditBookViewController.swift
+++ Collections/Presentation/Presentation-Domain/Screen/Book/EditBook/EditBookViewController.swift
@@ -19,6 +19,7 @@ final class EditBookViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTextField()
+        setupButton()
     }
 }
 
@@ -27,6 +28,51 @@ extension EditBookViewController {
     private func setupTextField() {
         [bookTitleTextField, bookPriceTextField, bookPurchaseDateTextField]
             .forEach { $0?.delegate = self }
+    }
+
+    private func setupButton() {
+        imageSelectButton.addTarget(
+            self,
+            action: #selector(setupPhotoLibrary),
+            for: .touchUpInside
+        )
+
+        takingPictureButton.addTarget(
+            self,
+            action: #selector(setupLaunchCamera),
+            for: .touchUpInside
+        )
+    }
+
+    @objc private  func tappedAddBookButton(_ sender: UIButton) {
+        
+    }
+
+    @objc private func tappedDoneButton(_ sender: UIButton) {
+        bookPurchaseDateTextField.text = DateFormatter.convertToYearAndMonth(UIDatePicker.purchaseDatePicker.date)
+        bookPurchaseDateTextField.endEditing(true)
+    }
+
+    @objc private func setupPhotoLibrary(_ sender: UIButton) {
+        let photoLibrary = UIImagePickerController.SourceType.photoLibrary
+
+        if UIImagePickerController.isSourceTypeAvailable(photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }
+    }
+
+    @objc private func setupLaunchCamera(_ sender: UIButton) {
+        let camera = UIImagePickerController.SourceType.camera
+
+        if UIImagePickerController.isSourceTypeAvailable(camera) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }
     }
 }
 
@@ -45,5 +91,23 @@ extension EditBookViewController: UITextFieldDelegate {
             textField.resignFirstResponder()
         }
         return true
+    }
+}
+
+extension EditBookViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
+        if let image = info[.originalImage] as? UIImage {
+            bookImageView.contentMode = .scaleAspectFill
+            bookImageView.image = image
+        }
+        self.dismiss(animated: true)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true)
     }
 }
