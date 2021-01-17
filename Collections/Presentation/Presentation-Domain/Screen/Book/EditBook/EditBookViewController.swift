4fcec4cff75a0498d290c028e98244d26ae3b2d0
yossibank <yossibank@yahoo.co.jp>
2021-01-17 09:59:57 +0900

    追加: 書籍編集アウトレット接続

--- Collections/Presentation/Presentation-Domain/Screen/Book/EditBook/EditBookViewController.swift
+++ Collections/Presentation/Presentation-Domain/Screen/Book/EditBook/EditBookViewController.swift
@@ -2,6 +2,16 @@ import UIKit
 
 final class EditBookViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var imageSelectButton: UIButton!
+    @IBOutlet weak var takingPictureButton: UIButton!
+    @IBOutlet weak var bookTitleTextField: UITextField!
+    @IBOutlet weak var bookPriceTextField: UITextField!
+    @IBOutlet weak var bookPurchaseDateTextField: UITextField!
+    @IBOutlet weak var validateTitleLabel: UILabel!
+    @IBOutlet weak var validatePriceLabel: UILabel!
+
     static func createInstance() -> EditBookViewController {
         EditBookViewController.instantiateInitialViewController()
     }
