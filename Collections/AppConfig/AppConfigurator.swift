566e727cf78ee1e476163168dbae310efb83f2ab
yossibank <yossibank@yahoo.co.jp>
2021-01-18 22:12:53 +0900

    作成: KeychainMangerクラス

--- Collections/AppConfig/AppConfigurator.swift
+++ Collections/AppConfig/AppConfigurator.swift
@@ -1,9 +1,13 @@
 import  Foundation
 
 final class AppConfigurator {
+    
+    private struct Constant {
+        static let apiUrl = "http://54.250.239.8"
+    }
 
     static var currentApiUrl: URL {
-        let path: URL? = URL(string: "http://54.250.239.8")
+        let path: URL? = URL(string: Constant.apiUrl)
         return path ?? URL(string: .blank)!
     }
 }
