387d687c20895a3cb85e294a94082079967fd0d9
yossibank <yossibank@yahoo.co.jp>
2021-03-22 23:45:08 +0900

    チャットルームのキーボードのshow, hideによってviewをあげる

--- BookList/Presentation/Presentation-Domain/Screen/Chat/ChatRoom/ChatRoomViewController.swift
+++ BookList/Presentation/Presentation-Domain/Screen/Chat/ChatRoom/ChatRoomViewController.swift
@@ -2,14 +2,22 @@ import UIKit
 
 final class ChatRoomViewController: UIViewController {
 
+    private struct Constants {
+        static let accessoryHeight: CGFloat = 100
+        static let contentInset: UIEdgeInsets = .init(top: 60, left: 0, bottom: 0, right: 0)
+    }
+
     @IBOutlet weak var tableView: UITableView!
 
     private var viewModel: ChatRoomViewModel!
     private var dataSource: ChatRoomDataSource!
+    private var safeAreaBottom: CGFloat {
+        self.view.safeAreaInsets.bottom
+    }
 
     private lazy var keyboardAccessoryView: KeyboardAccessoryView = {
         let view = KeyboardAccessoryView()
-        view.frame = .init(x: 0, y: 0, width: view.frame.width, height: 50)
+        view.frame = .init(x: 0, y: 0, width: view.frame.width, height: Constants.accessoryHeight)
         view.delegate = self
         return view
     }()
@@ -27,6 +35,7 @@ final class ChatRoomViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        setupNotification()
         fetchChatMessages()
     }
 
@@ -45,10 +54,26 @@ final class ChatRoomViewController: UIViewController {
         tableView.dataSource = dataSource
         tableView.estimatedRowHeight = 80
         tableView.rowHeight = UITableView.automaticDimension
-        tableView.contentInset = .init(top: 60, left: 0, bottom: 0, right: 0)
-        tableView.scrollIndicatorInsets = .init(top: 60, left: 0, bottom: 0, right: 0)
-        tableView.keyboardDismissMode = .interactive
+        tableView.contentInset = Constants.contentInset
+        tableView.scrollIndicatorInsets = Constants.contentInset
         tableView.transform = .init(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: 0)
+        tableView.keyboardDismissMode = .onDrag
+    }
+
+    private func setupNotification() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
     }
 
     private func fetchChatMessages() {
@@ -69,6 +94,24 @@ final class ChatRoomViewController: UIViewController {
                 self.tableView.reloadData()
             }
         }
+    }
+
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        if let endFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            guard endFrame.height >= Constants.accessoryHeight else { return }
+
+            let top = endFrame.height - safeAreaBottom
+            let contentInset = UIEdgeInsets(top: top, left: 0, bottom: 0, right: 0)
+
+            tableView.contentInset = contentInset
+            tableView.scrollIndicatorInsets = contentInset
+            tableView.contentOffset = .init(x: 0, y: -top)
+        }
+    }
+
+    @objc private func keyboardWillHide() {
+        tableView.contentInset = Constants.contentInset
+        tableView.scrollIndicatorInsets = Constants.contentInset
     }
 }
 
