3cd37693e96779b8707ad3a47f8b83edb8a4be71
yossibank <yossibank@yahoo.co.jp>
2021-07-11 13:45:35 +0900

    develop: プロジェクト名変更

--- Training1-SwiftTests/Training1-SwiftTests.swift
+++ Training1-SwiftTests/Training1-SwiftTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import BookManager
+@testable import Training1_Swift
 
-class BookListTests: XCTestCase {
+class Training1SwiftTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
